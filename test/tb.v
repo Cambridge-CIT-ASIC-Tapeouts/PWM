50668a157c6be00764a68b8e234f1012b4c13772
soujanyacccir <soujanyabharatraj@gmail.com>
2026-06-18 11:43:15 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_pwm dut (
+  tt_um_pwm_cit dut (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
